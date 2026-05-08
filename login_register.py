290173939ad05c9304bb5e50441a39be8a9246bf
Albion Hoxha <albion.hoxha6@student.uni-pr.edu>
2026-05-08 23:18:46 +0200

    Add login authentication logic

--- login_register.py
+++ login_register.py
@@ -4,7 +4,9 @@ from database import get_connection
 from security import generate_salt, hash_password
 from dashboard import open_dashboard
 from database import init_db
+
 init_db()
+
 
 def register_user():
     username = entry_user.get()
@@ -20,10 +22,40 @@ def register_user():
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (username, password_hash, salt) VALUES (%s, %s, %s)", (username, hashed, salt))
+        cursor.execute(
+            "INSERT INTO users (username, password_hash, salt) VALUES (%s, %s, %s)",
+            (username, hashed, salt),
+        )
         conn.commit()
         messagebox.showinfo("Success", "User u krijua!")
     except:
         messagebox.showerror("Error", "Username ekziston!")
     finally:
         conn.close()
+
+
+def login():
+    username = entry_user.get()
+    password = entry_pass.get()
+
+    if not username or not password:
+        messagebox.showwarning("Warning", "Plotëso të gjitha fushat!")
+        return
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT password_hash, salt FROM users WHERE username=%s", (username,)
+    )
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        db_hash, salt = result
+        if hash_password(password, salt) == db_hash:
+            app.destroy()
+            open_dashboard()
+        else:
+            messagebox.showerror("Error", "Password gabim!")
+    else:
+        messagebox.showerror("Error", "User nuk ekziston!")

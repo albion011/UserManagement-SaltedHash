b3b11a369aa82967ddf78d62e17c397e855b1349
alba-jashanica <albajashanica70@gmail.com>
2026-05-08 20:17:46 +0200

    Update security.py

--- security.py
+++ security.py
@@ -4,3 +4,7 @@ import base64
 
 def generate_salt():
     return base64.b64encode(os.urandom(16)).decode('utf-8')
+
+def hash_password(password, salt):
+    combined = password + salt
+    return hashlib.sha256(combined.encode()).hexdigest()

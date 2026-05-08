bab73ae2bdc5c243bf8eb7132f0525b3f0b64362
Albina Haliti <albina.haliti4@student.uni-pr.edu>
2026-05-08 21:17:58 +0200

    Update database.py

--- database.py
+++ database.py
@@ -8,3 +8,17 @@ def get_connection():
         password="",
         database="user_management"
     )
+
+def init_db():
+    conn = mysql.connector.connect(
+        host="localhost",
+        port=3307,
+        user="root",
+        password=""
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS user_management")
+
+    conn.commit()
+    conn.close()
